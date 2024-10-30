29267dfa4b7c934dcb72db1955b416a315bd6ceb
timbarlaid <akalkunov2005@gmail.com>
2024-10-30 20:47:53 +0300

    запихнул все переменные в словарь

--- Kran_15/Kran_15.py
+++ Kran_15/Kran_15.py
@@ -17,92 +17,138 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, m
 from tqdm import tqdm #Библиотека для отображения индикатора прогресса при выполнении итераций в циклах
 from statsmodels.tsa.arima.model import ARIMA #Библиотека для статистического анализа и построения временных рядов.
 
+
+
+
 # Код для разделения на разные файлы для REZ ////////////////////////////////////////
 
 
-def csv_to_dataframe(file_path: str) -> DataFrame:
-    # Считываем данные из CSV
-    dataframe = pd.read_csv(file_path)
+def to_dataframe(file_path: str = None, dataframe: DataFrame = None) -> DataFrame:
+    if file_path:
+        try:
+            # Считываем данные из CSV
+            dataframe = pd.read_csv(file_path)
 
-    if  not 'Datetime' in dataframe:
-        # Объединяем столбцы 'Дата' и 'Время' в один столбец 'Datetime'
-        dataframe['Datetime'] = pd.to_datetime(dataframe['Дата'] + ' ' + dataframe['Время'], format='%y-%m-%d %H:%M:%S.%f')
+            # Объединяем столбцы 'Дата' и 'Время' в один столбец 'Datetime'
+            dataframe['Datetime'] = pd.to_datetime(dataframe['Дата'] + ' ' + dataframe['Время'],
+                                                   format='%y-%m-%d %H:%M:%S.%f')
 
-        # Удаляем столбцы 'Дата' и 'Время', т.к. теперь мы их объединили в один 'Datetime'
-        dataframe.drop(columns=['Дата', 'Время'], inplace=True)
+            # Удаляем столбцы 'Дата' и 'Время', так как они больше не нужны
+            dataframe.drop(columns=['Дата', 'Время'], inplace=True)
 
-    else:
+        except Exception:
+            print(f"Ошибка при чтении CSV файла")
+            return None
+
+    # Если столбец 'Datetime' существует в предоставленном DataFrame
+
+    if dataframe is not None and 'Datetime' in dataframe.columns:
+        # Преобразуем столбец 'Datetime' в формат datetime
         dataframe['Datetime'] = pd.to_datetime(dataframe['Datetime'])
+        dataframe.set_index('Datetime', inplace=True)
 
-    # Устанавливаем 'Datetime' как индекс
-    dataframe.set_index('Datetime', inplace=True)
-
-    # Сортируем DataFrame по индексу (по времени)
+    # Сортируем DataFrame по индексу (времени)
     dataframe.sort_index(inplace=True)
 
-    # Преобразование индекса в формат времени только по дням 11111
-    dataframe.index = dataframe.index.map(lambda x: x.replace(hour=0, minute=0, second=0, microsecond=0))
+    # Изменяем индекс, чтобы он содержал только дату
+    if dataframe is not None:
+        dataframe.index = dataframe.index.normalize()
 
     return dataframe
 
 
 #создаем DataFrame
-rz = csv_to_dataframe('Кран/Rez/Rez_Month.csv')
+rz = to_dataframe(file_path = 'Кран/Rez/Rez_Month.csv')
+
+allGr = {}
 
 # Сохранение данных в отдельные CSV файлы для каждого статуса
 for rez, group in rz.groupby('Результат'):
     #По какой переменной будет подсчет(по дням = D)
     rez_counts = group.groupby(pd.Grouper(freq='D')).size()# Группируем данные по дням и подсчитываем количество записей в столбце 'ID'
-    rez_counts.to_csv(f'Кран/Rez/{rez}_Month.csv', header=[rez], index=True)#Запись файлов в папку
 
-# Чтение данных со всех файлов ///////////////////////////////////////////
+    if rez.endswith(':'):
+        rez = rez[:-1]
 
-#создаем DataFrame
-ID = csv_to_dataframe('Кран/ID_Month.csv')
+    rez_df = rez_counts.to_frame(name= rez )
+    allGr[rez] = to_dataframe(dataframe = rez_df)
 
-# Группируем данные по дням и подсчитываем количество записей в столбце 'ID'
-ID = ID['ID'].groupby(pd.Grouper(freq='D')).count()
-
-
-WAR = csv_to_dataframe('Кран/Rez/WAR_Month.csv')  # Считываем данные из CSV-файлов
-
-SUC = csv_to_dataframe('Кран/Rez/SUC_Month.csv') # Считываем данные из CSV-файлов
-
-S_OK = csv_to_dataframe('Кран/Rez/S_OK_Month.csv') # Считываем данные из CSV-файлов
-
-ERR = csv_to_dataframe('Кран/Rez/ERR_Month.csv') # Считываем данные из CSV-файлов
-
-# Выбор кол-ва данных для прогноза в % , заполнение массивов данными ///////////////////////////////////////////
-
-percentsize=1 #100%
-
-#массив для всех файлов
-allGr = [
-    ID[:int(len(ID) * percentsize)],
-    WAR[:int(len(WAR) * percentsize)],
-    SUC[:int(len(SUC) * percentsize)],
-    S_OK[:int(len(S_OK) * percentsize)],
-    ERR[:int(len(ERR) * percentsize)]
-]
-#массив имен для всех файлов
-nameGr=['id','WAR','SUC','S_OK','ERR']
-
-# Выбор кол-ва данных для прогноза в % , заполнение массивов данными ///////////////////////////////////////////
-
-date_form = mdates.DateFormatter("%d-%m") # Устанавливаем формат даты для графиков в виде "день-месяц"
+    print(allGr[rez])
 
 
-fig, ax = plt.subplots(figsize=(12, 8)) # Создаем новую фигуру и ось для графиков
-
-# Строим каждый график
-for graf, nameG in zip(allGr, nameGr): #данные берутся из заданных массивов в начале
-    graf.plot(ax=ax, label=nameG)
 
 
-ax.set_xlabel('Дни') # Задаем название оси x как 'Дни'
-ax.xaxis.set_major_formatter(date_form) # Устанавливаем основной формат даты на оси x с использованием ранее заданного формата
-ax.xaxis.set_major_locator(mdates.DayLocator(interval=2))# Устанавливаем основной тип для оси x, чтобы показывать деления каждые 2 дня
-plt.legend() # Добавляем легенду на график
-plt.grid()# Включаем сетку на графике
-plt.tight_layout()# Автоматически корректируем плотность компонентов на графике для лучшего визуального восприятия
-plt.show() # Отображаем график
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #Устанавливаем процентный размер данных для анализа
+#
+# percentsize = 1  # 100% (берем все данные)
+#
+# # Создаем массив `allGr`, содержащий первые percentsize*100% данных из каждого массива (ID, WAR, SUC, S_OK, ERR)
+# allGr = [arr[:int(len(arr) * percentsize)] for arr in [ID, WAR, SUC, S_OK, ERR]]
+#
+# # Задаем имена для каждого массива данных, которые будут использоваться в легенде графика
+# nameGr = ['id', 'WAR', 'SUC', 'S_OK', 'ERR']
+#
+# # Настраиваем формат отображения даты на оси X и создаем фигуру и ось для графиков
+# fig, ax = plt.subplots(figsize=(12, 8))  # Создаем фигуру размером 12x8 дюймов
+# date_form = mdates.DateFormatter("%d-%m")  # Устанавливаем формат даты для оси X: "день-месяц"
+#
+# # Строим график для каждого массива данных из `allGr` и присваиваем имя из `nameGr`
+# for graf, name in zip(allGr, nameGr):
+#     ax.plot(graf, label=name)  # Добавляем линию для текущего массива данных с меткой `name`
+#
+# # Настраиваем подписи и формат оси X
+# ax.set_xlabel('Дни')  # Подписываем ось X как "Дни"
+# ax.xaxis.set_major_formatter(date_form)  # Применяем формат даты к основной оси X
+# ax.xaxis.set_major_locator(mdates.DayLocator(interval=2))  # Устанавливаем деления по оси X каждые 2 дня
+#
+# # Отображаем дополнительные элементы графика
+# plt.legend()  # Добавляем легенду, чтобы обозначить каждый массив данных
+# plt.grid()  # Включаем сетку для лучшей читаемости графика
+# plt.tight_layout()  # Автоматически регулируем компоненты графика для минимизации пустого пространства
+# plt.show()  # Отображаем график
